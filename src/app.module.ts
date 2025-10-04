022f39ce0af6604ddb9621dd6e5cdadb27974710
Vicente Simón <simonsamper@gmail.com>
2025-10-04 20:51:27 +0200

    Antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 @Module( {
@@ -17,6 +18,8 @@ import { CommonModule } from './common/common.module';
     PokemonModule,
 
     CommonModule,
+
+    SeedModule,
   ],
 
 } )
